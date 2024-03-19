d072eac1acb87c600e4b3afcaacef8f704d6474e
nguyen-ha-01 <aa12nguyenha@gmail.com>
2024-03-19 23:02:06 +0700

    fix: format code

--- lib/app.dart
+++ lib/app.dart
@@ -1,42 +1,22 @@
 import 'package:flutter/material.dart';
 import 'package:qr_code_scanner/common/widgets/item_qr.dart';
-
-import 'flavors.dart';
+import 'package:qr_code_scanner/flavors.dart';
 
 class App extends StatelessWidget {
   const App({Key? key}) : super(key: key);
 
   @override
-  Widget build(BuildContext context) {
-    return MaterialApp(
-      title: F.title,
-      theme: ThemeData(
-        primarySwatch: Colors.blue,
-      ),
-      home: Center(
-        child: ItemQr(
-          iconSrc: "assets/icons/icon_wifi.svg",
-          textValue: "wifi",
-          onTap: () {},
+  Widget build(BuildContext context) => MaterialApp(
+        title: F.title,
+        theme: ThemeData(
+          primarySwatch: Colors.blue,
         ),
-      ),
-    );
-  }
-
-  Widget _flavorBanner({
-    required Widget child,
-    bool show = true,
-  }) =>
-      show
-          ? Banner(
-              child: child,
-              location: BannerLocation.topStart,
-              message: F.name,
-              color: Colors.green.withOpacity(0.6),
-              textStyle: TextStyle(fontWeight: FontWeight.w700, fontSize: 12.0, letterSpacing: 1.0),
-              textDirection: TextDirection.ltr,
-            )
-          : Container(
-              child: child,
-            );
+        home: Center(
+          child: ItemQr(
+            iconSrc: 'assets/icons/icon_wifi.svg',
+            textValue: 'wifi',
+            onTap: () {},
+          ),
+        ),
+      );
 }
