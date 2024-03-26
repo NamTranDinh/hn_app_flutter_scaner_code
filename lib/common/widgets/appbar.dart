ba703ee8c59f5b13901314baa99fd627f6bd764d
Tran Dinh Nam <namtd@aht-asia.com>
2024-03-26 22:16:28 +0700

    fix: incorrect parent widget

--- lib/common/widgets/appbar.dart
+++ lib/common/widgets/appbar.dart
@@ -14,50 +14,48 @@ class CustomAppBar extends StatelessWidget implements PreferredSizeWidget {
   @override
   Widget build(BuildContext context) => SafeArea(
         child: Container(
+          padding: const EdgeInsets.symmetric(horizontal: 34),
           height: kToolbarHeight,
           color: AppColor.grey,
           alignment: Alignment.center,
-          child: Expanded(
-            child: Row(
-              mainAxisAlignment: MainAxisAlignment.spaceBetween,
-              crossAxisAlignment: CrossAxisAlignment.center,
-              children: [
-                Text(
+          child: Row(
+            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+            crossAxisAlignment: CrossAxisAlignment.center,
+            children: [
+              Expanded(
+                child: Text(
                   title,
                   style: AppTextStyle.title1.copyWith(
-                      color: AppColor.blackD9,
-                      fontSize: 27,
-                      fontWeight: FontWeight.w400,
-                      overflow: TextOverflow.ellipsis),
+                    color: AppColor.blackD9,
+                    fontSize: 27,
+                    fontWeight: FontWeight.w400,
+                    overflow: TextOverflow.ellipsis,
+                  ),
                 ),
-                const Spacer(),
-                Padding(
-                  padding: const EdgeInsets.only(right: 31.0),
-                  child: GestureDetector(
-                    onTap: action,
-                    child: Container(
-                      width: 40,
-                      height: 40,
-                      decoration: BoxDecoration(
-                        boxShadow: [
-                          BoxShadow(color: AppColor.pureBlack.withAlpha((255 * 0.6).toInt()), blurRadius: 20),
-                        ],
-                        color: AppColor.grey,
-                        borderRadius: BorderRadius.circular(6),
-                      ),
-                      child: Padding(
-                        padding: const EdgeInsets.all(10.0),
-                        child: SvgPicture.asset(
-                          Assets.icons.menu,
-                          width: 24,
-                          height: 24,
-                        ),
-                      ),
+              ),
+              GestureDetector(
+                onTap: action,
+                child: Container(
+                  width: 40,
+                  height: 40,
+                  decoration: BoxDecoration(
+                    boxShadow: [
+                      BoxShadow(color: AppColor.pureBlack.withAlpha((255 * 0.6).toInt()), blurRadius: 20),
+                    ],
+                    color: AppColor.grey,
+                    borderRadius: BorderRadius.circular(6),
+                  ),
+                  child: Padding(
+                    padding: const EdgeInsets.all(10.0),
+                    child: SvgPicture.asset(
+                      Assets.icons.menu,
+                      width: 24,
+                      height: 24,
                     ),
                   ),
                 ),
-              ],
-            ),
+              ),
+            ],
           ),
         ),
       );
